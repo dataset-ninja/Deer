925c82d25f4bc5ecd22e1e1de25f77f715133f8f
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-06-19 14:54:05 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -17,26 +17,26 @@ CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
 ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_YEAR: int = 2022
-HOMEPAGE_URL: str = "https://universe.roboflow.com/deertracker/deer-yidlt"
+HOMEPAGE_URL: str = "https://universe.roboflow.com/galen-marsh-vvdvx/deer-z7klu"
 # e.g. "https://some.com/dataset/homepage"
 
 PREVIEW_IMAGE_ID: int = 642044
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = "https://github.com/dataset-ninja/Deer"
+GITHUB_URL: str = "https://github.com/dataset-ninja/deer-z7klu"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://universe.roboflow.com/deertracker/deer-yidlt/dataset/1/download"
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://universe.roboflow.com/galen-marsh-vvdvx/deer-z7klu/dataset/3/download"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # If specific colors for classes are needed, fill this dict (e.g. {"class1": [255, 0, 0], "class2": [0, 255, 0]})
 
 PAPER: Optional[str] = None
-CITATION_URL: Optional[str] = "https://universe.roboflow.com/deertracker/deer-yidlt"
+CITATION_URL: Optional[str] = "https://universe.roboflow.com/galen-marsh-vvdvx/deer-z7klu"
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Roboflow community"
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://universe.roboflow.com/galen-marsh-vvdvx"
 TAGS: List[str] = None
